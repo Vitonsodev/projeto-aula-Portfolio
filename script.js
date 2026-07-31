877f8214aa1d7cce08627cfe55b313745ceed46c
VitonsoDev <vitonsodias@gmail.com>
2026-07-31 00:18:19 -0300

    Start GitHub API integration for user, repositories, and language requests

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-fetch("https://api.github.com/users/Vitonsodev")
+fetch('https://api.github.com/users/${usuario}')
+    .then(resposta => resposta.json())
+    .then(dados=>{
 
-let menu = document.getElementById("menuNavegacao")
-let trilho = document.getElementById("trilho");
+    });
+
+let menu = document.getElementById("menuNavegacao") //=====================================//
+let trilho = document.getElementById("trilho");      // Variáveis para tema claro e escuro//
 let body = document.querySelector("body")
-let iconeTema = document.getElementById("iconeTema")
+let iconeTema = document.getElementById("iconeTema")//====================================//
 
 trilho.addEventListener("click", () => {
     body.classList.toggle("dark");
@@ -14,7 +18,15 @@ trilho.addEventListener("click", () => {
     }
 });
 
+const inputUsuario = document.getElementById("nomeUsuario");
+const pefilGithub = document.getElementById("perfilGithub");
 
+function buscarUsuario(){
+    const usuario = inputUsuario.value;
+
+    console.log(usuario)
+
+}
 
 
 
